193878629599f07103df51adf87a1cf622656981
OliveNsu <tasbiul.hasan@northsouth.edu>
2023-08-08 13:20:46 +0600

    change button

--- src/Components/LoginSystem/Register.js
+++ src/Components/LoginSystem/Register.js
@@ -11,6 +11,7 @@ const Register = () => {
     const [email, setEmail] = useState('');
     const [phone, setPhone] = useState('');
     const [showPassword, setShowPassword] = useState(false);
+    const [showConfirmPassword, setConfirmShowPassword] = useState(false);
     const [password, setPassword] = useState('');
     const [confirmpassword, setConfirmpassword] = useState('');
     const queryParams = new URLSearchParams(window.location.search);
@@ -79,6 +80,9 @@ const Register = () => {
     const handleShowPassword = () => {
       setShowPassword(!showPassword);
     };
+    const handleShowConfirmPassword = () => {
+      setConfirmShowPassword(!showConfirmPassword);
+    };
     
     return (
         <div>
@@ -141,13 +145,26 @@ const Register = () => {
               </button>
                 </div>
                 
+                <div className='flex m-auto max-w-xs'>
                 <input
-                  type="password"
+                  type={showConfirmPassword ? 'text' : 'password'}
                   name='confirmpassword'
                   placeholder="Type your confirmpassword" required
                   onChange={(e)=>{setConfirmpassword(e.target.value)}}
-                  className="m-5 block mx-auto input w-full max-w-xs"
+                  className="m-5 mx-auto input w-full max-w-xs"
                 />
+                <button className='btn mt-5' type="button" onClick={handleShowConfirmPassword}>
+                {showConfirmPassword ? <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z" />
+  <path strokeLinecap="round" strokeLinejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+</svg>
+
+ : <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+ <path strokeLinecap="round" strokeLinejoin="round" d="M3.98 8.223A10.477 10.477 0 001.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.45 10.45 0 0112 4.5c4.756 0 8.773 3.162 10.065 7.498a10.523 10.523 0 01-4.293 5.774M6.228 6.228L3 3m3.228 3.228l3.65 3.65m7.894 7.894L21 21m-3.228-3.228l-3.65-3.65m0 0a3 3 0 10-4.243-4.243m4.242 4.242L9.88 9.88" />
+</svg>
+}
+              </button>
+                </div>
                 <p className=''>Referral Code:</p>
   <input type="text" value={referralCode || ''} readOnly className="input input-bordered w-full max-w-xs" />
                 {/* <label>Referral Code:</label>
