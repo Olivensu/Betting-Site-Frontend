1a0cf483e384951f45a4f2fabc8234887c105bb0
OliveNsu <tasbiul.hasan@northsouth.edu>
2023-08-12 10:42:04 +0600

    home button remove

--- src/Components/Shared/Header.js
+++ src/Components/Shared/Header.js
@@ -12,18 +12,18 @@ const Header = () => {
     return <Loading></Loading>;
 }
     return (
-        <div className='grid grid-cols-4 bg-[#F1C93B]'>
+        <div className='grid grid-cols-3 bg-[#F1C93B]'>
             <Link className='m-auto' to='/'>
             <img className='w-32  h-16 p-2 rounded-xl' src={icon} alt="" />
             </Link>
-            <Link className='m-auto' to='/'>
+            {/* <Link className='m-auto' to='/'>
             <button className='text-center text-xl'>
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 m-auto">
   <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
 </svg>
 Home
             </button>
-            </Link>
+            </Link> */}
             {
               user? <Link  className='m-auto' to='/user'>
               <button className='text-center text-xl'>
